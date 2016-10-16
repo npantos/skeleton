d5664523a8f563e0d9d987c80315e7a3ba7d6c6b
Nemanja Pantoš <npantos@gmail.com>
2016-10-17 01:10:00 +0200

    Comments added

--- js/custom.js
+++ js/custom.js
@@ -49,3 +49,65 @@ $(document).ready(function() {
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+/*------------------------------------*\
+    Isotope.js
+\*------------------------------------*/
+
+// init Isotope and resize images
+var $grid = $('.grid')
+$(window).load(function() {
+    resizeBoxes();
+    initIsotope();
+});
+
+function resizeBoxes()
+{
+    var h = 0;
+    $('div.view').each(function() 
+    {
+        var b = $(this);
+        if (h < b.height()) h = b.height();
+    });     
+    $('div.view').height(h);
+} 
+function initIsotope(){
+$grid.isotope({
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows'
+});  
+}
+
+// store filter for each group
+var filters = {};
+
+$('.filters').on( 'click', '.btn', function() {
+  var $this = $(this);
+  // get group key
+  var $buttonGroup = $this.parents('.button-group');
+  var filterGroup = $buttonGroup.attr('data-filter-group');
+  // set filter for group
+  filters[ filterGroup ] = $this.attr('data-filter');
+  // combine filters
+  var filterValue = concatValues( filters );
+  // set filter for Isotope
+  $grid.isotope({ filter: filterValue });
+});
+
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
+  
+// flatten object by concatting values
+function concatValues( obj ) {
+  var value = '';
+  for ( var prop in obj ) {
+    value += obj[ prop ];
+  }
+  return value;
+}
